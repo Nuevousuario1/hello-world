f3d4a03ed920291638fbd605472f75959aeb9f98
Nuevousuario1 <kljkhggtyftgv@yopmail.com>
2016-04-21 03:23:09 -0700

    Added files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hola mundo :)
+            /* nose que estoy haciendo
+               :( */
+            Double a = 2;
+            Decimal l = 0.0m;
+            int b = 6;
+            float c = 4.1f;
+            b= (int)l;
+            Console.WriteLine("hola mundo = "+ a*b);
+            Console.WriteLine("el valor de f es = {0:C}", b);
+            Console.WriteLine("ejemplo" +  l);
+            Console.ReadKey();
+
+        }
+    }
+}
